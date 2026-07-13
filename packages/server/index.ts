209e5c94026dd65f25e515ebb033323da152e657
Anthony Savchenko <anthony.savchenko@gmail.com>
2026-07-13 00:56:48 -0700

    Creating the Frontend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send(process.env.OPENAI_API_KEY || "Hello, World!");
+  res.send("Hello, World!");
 });
 
 app.listen(port, () => {
